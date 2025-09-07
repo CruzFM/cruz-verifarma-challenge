d30e30b8bf0a1761cf2c67bebf6da330b744f189
Fernando Cruz <cruzfernandomartin94@gmail.com>
2025-09-07 11:41:56 -0300

    F329 - Feat: initial setup. Tailwind and DaisyUI installed successfully for styling purposes.

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn bg-green-600">Primary</button>
+  </div>
+</template>
