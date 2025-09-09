28957e897372a3fb2f0bb4f6d0d8917880a3970b
Fernando Cruz <cruzfernandomartin94@gmail.com>
2025-09-09 00:08:45 -0300

    F-336 - Feat: Basic hero section. To-do: button reusable component.

---
+++ app/components/Hero.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<template>
+    <div class="hero bg-[#2A2A2A] text-white md:h-96 rounded-lg">
+        <div class="hero-content ">
+            <div class="max-w-md">
+            <h1 class="text-5xl font-bold  ">Veriflix video club</h1>
+            <p class="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <!-- To-do: this button HAS to be a component itself  -->
+            <button class="btn btn-primary bg-[#01CC90] text-black border-none">Browse</button>
+            </div>
+        </div>
+    </div>
+</template>
--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,5 +1,7 @@
 <script lang="ts">
 </script>
 <template>
-    <div>I'm the HOME page</div>
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-4 space-y-6">
+      <Hero />
+    </div>
 </template>
